d72a510accc723d79949e58d922777b367a45184
Dexter <igorilic03@gmail.com>
2019-10-23 19:42:21 +0200

    Working hard or hardly working?

--- Hard_SoftApplication/Hard_SoftApplication/Hard_SoftApplication.py
+++ Hard_SoftApplication/Hard_SoftApplication/Hard_SoftApplication.py
@@ -1,16 +1,17 @@
 import speech_recognition as sr
-import keyboard
 from HelperFunctions import listMicrophones
 
 r = sr.Recognizer()
-#listMicrophones()
+listMicrophones()
 while True:
     with sr.Microphone(device_index=1) as source:
         print("Speak Anything :")
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
+        r.adjust_for_ambient_noise(source, duration=0.5)
+        audio = r.listen(source, phrase_time_limit=8)
         try:
-            text = r.recognize_google(audio)
+            with open(r"C:\Users\Dexter\Desktop\Coding\Hard&Soft\Hard_SoftApplication\HardSoftApplication-7446dbcb9de9.json", "r") as f:
+                credentials = f.read()
+            text = r.recognize_google_cloud(audio, credentials_json= credentials)
             print("You said : {}".format(text))
         except:
             print("Sorry could not recognize what you said")
