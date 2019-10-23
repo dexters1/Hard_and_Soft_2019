664fbec37c2454e471095c45aaeb27beb366ac2d
Dexter <igorilic03@gmail.com>
2019-10-23 20:06:49 +0200

    credentials fix

--- Hard_SoftApplication/Hard_SoftApplication/Hard_SoftApplication.py
+++ Hard_SoftApplication/Hard_SoftApplication/Hard_SoftApplication.py
@@ -9,7 +9,7 @@ while True:
         r.adjust_for_ambient_noise(source, duration=0.5)
         audio = r.listen(source, phrase_time_limit=8)
         try:
-            with open(r"C:\Users\Dexter\Desktop\Coding\Hard&Soft\Hard_SoftApplication\HardSoftApplication-7446dbcb9de9.json", "r") as f:
+            with open(r"C:\Users\Dexter\Downloads\hardsoftapplication-52182c5dd661.json", "r") as f:
                 credentials = f.read()
             text = r.recognize_google_cloud(audio, credentials_json= credentials)
             print("You said : {}".format(text))
